349d5390da95222b9474bed37ee9ffbfa63cb9e2
Smitkhobragade <100017129+Smitkhobragade@users.noreply.github.com>
2023-04-27 22:53:20 +0530

    Added Analytics and Newsletter Section

--- data-finance/src/App.js
+++ data-finance/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "./Components/Navbar1";
 import Hero from "./Components/Hero";
+import Analytics from "./Components/Analytics";
+import Newsletter from "./Components/Newsletter";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Analytics />
+      <Newsletter />
+
     </div>
   );
 }
